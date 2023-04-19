766f8a7e07c9ff6b6d76fdaa7865452558fde4ed
hardkill551 <hardkill194@gmail.com>
2023-04-18 21:51:07 -0300

    Inicio do exercício

---
+++ src/app.js
@@ -0,0 +1,34 @@
+// cabeçalho para incluir printf, scanf e fgets
+// em programas javascript
+
+var scanf_stub = require('scanf-printf-obi').scanf,
+    printf_stub = require('scanf-printf-obi').sprintf,
+    fgets_stub = require('scanf-printf-obi').fgets
+
+function scanf(format) {
+    var res = scanf_stub.apply(this, Array.prototype.slice.call(arguments, 0));
+    eval(res);
+}
+
+function printf(args){
+    var res = printf_stub.apply(this, Array.prototype.slice.call(arguments, 0));
+    process.stdout.write(res);
+}
+
+function fgets(args){
+    var res = fgets_stub.apply(this, Array.prototype.slice.call(arguments, 0));
+    eval(res);
+}
+// final de cabeçalho
+
+
+
+let users, password;
+const passwords = []
+scanf("%d", users)
+console.log(users)
+for (let i = 0; i < users; i++) {
+    scanf("%s", password)
+    console.log(password)
+    passwords.push(password)
+}
